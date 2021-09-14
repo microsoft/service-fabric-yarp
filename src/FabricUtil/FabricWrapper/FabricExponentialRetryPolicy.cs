9ca8e64525ca382c67779d6ae9f5926df43d06ab
Artem Golotin <artem.golotin@gmail.com>
2021-09-14 11:32:52 -0700

    Fabric api call retries and additional fixes

---
+++ src/FabricUtil/FabricWrapper/FabricExponentialRetryPolicy.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace IslandGateway.ServiceFabricIntegration
+{
+    /// <summary>
+    /// Structure providing the defaults for SF retry policy.
+    /// </summary>
+    public struct FabricExponentialRetryPolicy
+    {
+        /// <summary>
+        /// Default intance of the policy.
+        /// </summary>
+        public static readonly FabricExponentialRetryPolicy Default = new()
+        {
+            NumAttempts = 3,
+            InitialBackoffMs = 1000,
+            MaxBackoffMs = 15000,
+        };
+
+        /// <summary>
+        /// Total number of attempts allowed in the retry policy.
+        /// </summary>
+        public uint NumAttempts { get; init; }
+
+        /// <summary>
+        /// Initial retry backoff.
+        /// </summary>
+        public uint InitialBackoffMs { get; init; }
+
+        /// <summary>
+        /// Maximum time allowed for a backoff.
+        /// </summary>
+        public uint MaxBackoffMs { get; init; }
+
+        /// <summary>
+        /// Helper function to determine whether retry may be allowed by policy.
+        /// On positive evaluation, next backoff is populated appropriately.
+        /// </summary>
+        /// <param name="attempt">Current attempt. Must be >= 1.</param>
+        /// <param name="backoffBeforeNextAttempt">The next backoff.</param>
+        /// <returns>Boolean whether retry is allowed by policy.</returns>
+        public bool IsRetryAllowed(int attempt, out int backoffBeforeNextAttempt)
+        {
+            backoffBeforeNextAttempt = default;
+            if (attempt <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(attempt));
+            }
+            if (attempt < this.NumAttempts)
+            {
+                backoffBeforeNextAttempt = (int)Math.Min((Math.Pow(2, attempt) - 1) * this.InitialBackoffMs, this.MaxBackoffMs);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ test/FabricDiscovery.Core.Tests/Service/Util/FabricCallHelperTests.cs
@@ -0,0 +1,154 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using IslandGateway.ServiceFabricIntegration;
+using Xunit;
+
+namespace IslandGateway.FabricDiscovery.Util.Tests
+{
+    public class FabricCallHelperTests
+    {
+        [Fact]
+        public async Task RunWithRetries_FirstAttemptSucceeds()
+        {
+            // Arrange
+            var attempts = new List<int>();
+
+            // Act
+            var result = await FabricCallHelper.RunWithExponentialRetries(
+                async (attempt, _) =>
+                {
+                    attempts.Add(attempt);
+
+                    await Task.Yield();
+                    return "ok";
+                },
+                FabricExponentialRetryPolicy.Default,
+                CancellationToken.None);
+
+            // Assert
+            attempts.Should().Equal(new[] { 1 });
+            result.Should().Be("ok");
+        }
+
+        [Fact]
+        public async Task RetryTransientFailures_RetriesTransientErrors()
+        {
+            // Arrange
+            var attempts = new List<int>();
+
+            // Act
+            var result = await FabricCallHelper.RunWithExponentialRetries(
+                async (attempt, _) =>
+                {
+                    attempts.Add(attempt);
+
+                    await Task.Yield();
+                    if (attempt == 1)
+                    {
+                        throw new TimeoutException("transient");
+                    }
+                    else if (attempt == 2)
+                    {
+                        throw new FabricTransientException();
+                    }
+
+                    return "ok";
+                },
+                FabricExponentialRetryPolicy.Default,
+                CancellationToken.None);
+
+            // Assert
+            attempts.Should().Equal(new[] { 1, 2, 3 });
+            result.Should().Be("ok");
+        }
+
+        [Fact]
+        public async Task RetryTransientFailures_DoesNotRetryNonTransient()
+        {
+            // Arrange
+            var attempts = new List<int>();
+
+            // Act
+            Func<Task> func = () => FabricCallHelper.RunWithExponentialRetries<string>(
+                async (attempt, _) =>
+                {
+                    attempts.Add(attempt);
+
+                    await Task.Yield();
+                    throw new Exception("boom");
+                },
+                FabricExponentialRetryPolicy.Default,
+                CancellationToken.None);
+
+            // Assert
+            await func.Should().ThrowAsync<Exception>().WithMessage("boom");
+            attempts.Should().Equal(new[] { 1 });
+        }
+
+        [Fact]
+        public async Task RetryTransientFailures_TranslatesDeliberateCancellations()
+        {
+            // Arrange
+            var attempts = new List<int>();
+            using var cts = new CancellationTokenSource();
+            var tcs1 = new TaskCompletionSource<int>();
+            var tcs2 = new TaskCompletionSource<int>();
+
+            // Act
+            var task = FabricCallHelper.RunWithExponentialRetries<string>(
+                async (attempt, _) =>
+                {
+                    attempts.Add(attempt);
+
+                    await Task.Yield();
+                    tcs1.SetResult(0);
+
+                    await tcs2.Task;
+                    throw new FabricTransientException(FabricErrorCode.OperationCanceled);
+                },
+                FabricExponentialRetryPolicy.Default,
+                cts.Token);
+
+            await tcs1.Task;
+
+            cts.Cancel();
+            tcs2.SetResult(0);
+
+            Func<Task> func = () => task;
+
+            // Assert
+            await func.Should().ThrowAsync<OperationCanceledException>();
+            attempts.Should().Equal(new[] { 1 });
+        }
+
+        [Fact]
+        public async Task RunWithRetries_AbortsWhenCanceled()
+        {
+            // Arrange
+            var attempts = new List<int>();
+            using var cts = new CancellationTokenSource();
+            cts.Cancel();
+
+            // Act
+            Func<Task> func = () => FabricCallHelper.RunWithExponentialRetries<string>(
+                (attempt, _) =>
+                {
+                    attempts.Add(attempt);
+                    throw new Exception("Execvution should never get here.");
+                },
+                FabricExponentialRetryPolicy.Default,
+                cts.Token);
+
+            // Assert
+            await func.Should().ThrowAsync<OperationCanceledException>();
+            attempts.Should().BeEmpty();
+        }
+    }
+}
